f3aa4bdaa575ac94c67c7981bdd7bab2b67cf7db
softprops <d.tangren@gmail.com>
2019-09-27 19:44:42 +0900

    structopt setup

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use std::{error::Error, path::PathBuf, process::exit};
+use structopt::StructOpt;
+
+#[derive(StructOpt)]
+struct Opts {
+    path: PathBuf,
+}
+
+fn run(opts: Opts) -> Result<(), Box<dyn Error>> {
+    let Opts { path } = opts;
+    println!("{}", path.display());
+    Ok(())
+}
+
 fn main() {
-    println!("Hello, world!");
+    if let Err(err) = run(Opts::from_args()) {
+        eprintln!("{}", err);
+        exit(1)
+    }
 }
